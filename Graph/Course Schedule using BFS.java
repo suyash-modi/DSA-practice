ad3328c676575ca302eb43508d4b86371d6f2e94
suyash modi <134408224+suyash-modi@users.noreply.github.com>
2025-10-01 16:14:42 +0530

    Update and rename Course Schedule using BFS to Course Schedule using BFS.java

--- Graph/Course Schedule using BFS.java
+++ Graph/Course Schedule using BFS.java
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/course-schedule/
+// https://leetcode.com/problems/course-schedule/
 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
